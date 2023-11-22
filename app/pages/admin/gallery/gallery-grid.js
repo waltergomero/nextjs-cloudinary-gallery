bd791fe41db6f78c579d9f5fe6734bce60d57998
Walter Gomero <walter.gomero@hp.com>
2023-11-22 13:49:55 -0600

    updated gallery-grid page

--- app/pages/admin/gallery/gallery-grid.js
+++ app/pages/admin/gallery/gallery-grid.js
@@ -1,23 +1,64 @@
 "use client";
 
-import { ImageGrid } from "@/components/image-grid";
-import { CloudinaryImage } from "@/components/cloudinary-image";
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function GalleryGrid({ images }) {
+const GalleryGrid = ({images}) => {
   return (
-    <ImageGrid
-      images={images}
-      getImage={(images) => {
-        return (
-          <CloudinaryImage
-            key={images.public_id}
-            imageData={images}
-            width="400"
-            height="300"
-            alt="an image of something"
-          />
-        );
-      }}
-    />
-  );
+    <main className="mx-auto max-w-[1960px] p-4">
+    <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
+        
+        {images && images?.map((item) =>(
+                    <Link
+                    key={item.id}
+                    href={`/?photoId=${item.id}`}
+                    shallow
+                    className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+                  >
+              <Image
+              alt="Next.js Conf photo"
+              className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+              style={{ transform: 'translate3d(0, 0, 0)' }}
+              
+              src={item.url}
+              width={720}
+              height={480}
+              sizes="(max-width: 640px) 100vw,
+                (max-width: 1280px) 50vw,
+                (max-width: 1536px) 33vw,
+                25vw"
+            />
+            </Link>
+        ))
+            }
+        
+        </div>
+        </main>
+  )
 }
+
+export default GalleryGrid
+// "use client";
+
+// import { ImageGrid } from "@/components/image-grid";
+// import { CloudinaryImage } from "@/components/cloudinary-image";
+
+// export default function GalleryGrid({ images }) {
+//   return (
+//     <ImageGrid
+//       images={images}
+//       getImage={(images) => {
+//         return (
+//           <CloudinaryImage
+//             key={images.public_id}
+//             imageData={images}
+//             width="400"
+//             height="300"
+//             alt="an image of something"
+//           />
+//         );
+//       }}
+//     />
+//   );
+// }
